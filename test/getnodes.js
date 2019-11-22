5624ea7111eadc45a5d33127e50e1e0e13ad3ea0
Drew Letcher <drew@drewletcher.net>
2019-11-22 14:46:38 -0600

    parsing and rendering

--- test/getnodes.js
+++ test/getnodes.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
 const marked = require('marked');
+const PdfParser = require('../lib/pdfParser');
+const fs = require('fs');
 
-let md = fs.readFileSync('test/example.md', 'utf8');
+let md = fs.readFileSync('test/simple.md', 'utf8');
 
 let options = marked.defaults || {};
 fs.writeFileSync('test/options.json', JSON.stringify(options,null,"  "), 'utf8');
@@ -11,7 +12,10 @@ fs.writeFileSync('test/tokens.json', JSON.stringify(tokens,null,"  "), 'utf8');
 fs.writeFileSync('test/links.json', JSON.stringify(tokens.links, null, "  "), 'utf8');
 console.log(tokens.length);
 
-const parsed = marked.Parser.parse( tokens );
-fs.writeFileSync('test/parsed.html', parsed, 'utf8');
+//const html = marked.Parser.parse( tokens );
+//fs.writeFileSync('test/parsed.html', html, 'utf8');
+
+const docdef = PdfParser.parse( tokens );
+fs.writeFileSync('test/parsed.json', JSON.stringify(docdef,null,"  "), 'utf8');
 
 console.log(tokens.length);
--- test/simple.md
+++ test/simple.md
@@ -1,4 +1,10 @@
 # heading 1
 
 This is some text.
+This is some more **bold** text.
+
+---
+
+* item 1
+* item 2
 
